59860e39dff6a71efdf94118f97f42f3cf5199a0
DSC-LFIM\Jeans Graos <jgraos@unitru.edu.pe>
2021-01-22 10:44:11 -0500

    add project

---
+++ src/gatsby/config.ts
@@ -0,0 +1,22 @@
+// Fix import order later
+export default {
+  siteMetadata: {
+    title: `Alt`,
+    description: `Venda seus produtos online`,
+    author: `@lucis`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-use-query-params`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-antd`,
+    `gatsby-plugin-linaria`,
+    {
+      resolve: `gatsby-plugin-polyfill-io`,
+      options: {
+        features: [`IntersectionObserver`],
+      },
+    },
+  ],
+}
